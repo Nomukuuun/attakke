7b3d83a5200322d61f0891c0bc3c97fc713591da
nomukuuun <nomukuuun5@gmail.com>
2025-07-16 16:51:14 +0900

    Add: 外部認証用のカラムをuserに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_16_055015) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_16_074936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_16_055015) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
