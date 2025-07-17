4fc45b73dbb99816f469950b9f44a5b0a4bb2b31
nomukuuun <nomukuuun5@gmail.com>
2025-07-17 12:39:17 +0900

    Add: ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root 'top_pages#top'
+
+  devise_scope :user do
+    delete "logout", to: "users/sessions#destroy", as: :logout
+  end
 end
