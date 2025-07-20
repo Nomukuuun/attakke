f54efca4b4f257847ded28cb4658f8863fccdd16
nomukuuun <nomukuuun5@gmail.com>
2025-07-20 23:00:07 +0900

    Improve: stockモデルとhistoryモデルのカラムにenumを定義

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
 
+  enum status: { purchase: 0, consumption: 1 }
+
   has_many :stocks, dependent: :destroy
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,8 @@ class Stock < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :model, presence: true, length: { maximum: 255 }
 
+  enum model: { existence: 0, numbet: 1 }
+
   belongs_to :user
   belongs_to :location
   has_many :histories, dependent: :destroy
