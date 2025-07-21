9dbd28606a133c7971d407e465b3c9d916449e9d
nomukuuun <nomukuuun5@gmail.com>
2025-07-21 22:41:34 +0900

    Add: locationsテーブルにusersテーブルの参照を定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_20_121840) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_21_133236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_20_121840) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "stocks", force: :cascade do |t|
@@ -54,6 +56,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_20_121840) do
   end
 
   add_foreign_key "histories", "stocks"
+  add_foreign_key "locations", "users"
   add_foreign_key "stocks", "locations"
   add_foreign_key "stocks", "users"
 end
