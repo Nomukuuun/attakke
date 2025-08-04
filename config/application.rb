adff97a77495f88f80414b543edd5e9d34da2a2c
Nomukuuun <nomukuuun5@gmail.com>
2025-08-04 21:50:13 +0900

    Test/ Update application.rb
    renderデプロイでエラーを回避するために設定追加

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Myapp
       g.test_framework nil
     end
 
+    # 本番環境でbun installによるエラーを起こさないために追記
+    config.cssbundling.skip_css_install_on_assets_precompile = true
+    
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
   end
