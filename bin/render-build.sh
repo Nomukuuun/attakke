96343e24d4e37cadd80aacd7fc6477903f399901
Nomukuuun <nomukuuun5@gmail.com>
2025-08-04 23:31:21 +0900

    Test/Update render-build.sh
    renderデプロイエラー対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,6 @@ bundle install
 npm install
 npx @tailwindcss/cli -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify
 
-bundle exec rails tailwindcss:build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
