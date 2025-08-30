353413591a116ab3ab85ccdceaba1c1d3f78faec
nomukuuun <nomukuuun5@gmail.com>
2025-08-30 22:34:17 +0900

    Remove: usersテーブルからpartner_idのカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_18_053932) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_30_132720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_18_053932) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "partner_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
