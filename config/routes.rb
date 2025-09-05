28e9c7cf9905dc5ca4c97457aa61279823bce6d3
nomukuuun <nomukuuun5@gmail.com>
2025-09-05 15:30:13 +0900

    Add: Gmailの本番環境設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
