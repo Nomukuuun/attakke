eac299aa4ef6475fe5f2ad0a8a76cb49a952dfcb
nomukuuun <nomukuuun5@gmail.com>
2025-10-18 23:59:17 +0900

    ✨Add: stocksモデルに購入予定フラグを管理するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_28_135628) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_18_144042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_28_135628) do
     t.bigint "location_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "purchase_target", default: false, null: false
     t.index ["location_id"], name: "index_stocks_on_location_id"
     t.index ["user_id"], name: "index_stocks_on_user_id"
   end
