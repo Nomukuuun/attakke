18a10ddaf4b29add35a113151fd2bf85ac25881b
nomukuuun <nomukuuun5@gmail.com>
2025-10-20 12:12:47 +0900

    🔧fix: production.rbからconfig.hostsを削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,9 +110,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # 独自ドメイン設定
-  config.hosts << "mada-attakke.com"
-  config.hosts << "www.mada-attakke.com"
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
