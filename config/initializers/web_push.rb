2c3d7160ca29c953f9439e2438900e88603c0609
nomukuuun <nomukuuun5@gmail.com>
2025-10-23 09:16:50 +0900

    🗑️Remove: 不要なconfigファイルを削除

--- config/initializers/web_push.rb
+++
@@ -1,5 +0,0 @@
-# WebPush.configure do |config|
-#   config.vapid_public_key = ENV["VAPID_PUBLIC_KEY"]
-#   config.vapid_private_key = ENV["VAPID_PRIVATE_KEY"]
-#   config.vapid_subject = ENV["VAPID_SUBJECT"]
-# end
