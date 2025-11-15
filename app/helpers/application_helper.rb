dfb385fbf0bca75c628bc6e11493250ed3e98837
nomukuuun <nomukuuun5@gmail.com>
2025-11-15 16:17:01 +0900

    ♻️Refactor: パートナーシップにeditアクションを追加し、newアクションと責務を分離

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # パートナー設定画面へのパスを設定
+  def set_partnerships_path
+    current_user.reload
+    if current_user.active_partnership
+      edit_partnerships_path
+    else
+      new_partnerships_path
+    end
+  end
 end
