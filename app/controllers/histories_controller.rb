3f9156984e6cc429853d8af6a541c32bde9e6b49
nomukuuun <nomukuuun5@gmail.com>
2025-12-05 13:46:12 +0900

    ♻️Refactor: histories_controllerのロジックを見直し

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,19 +1,14 @@
 # ベース画面上でチェックボックス型の履歴を更新できるようにするためのコントローラ
-# 履歴は新規作成のみでストックと同時に削除される
 
 class HistoriesController < ApplicationController
   include Broadcast
 
   def create
-    latest_history = History.latest
-    set_stock_reflected_latest_history(latest_history)
-    save_quantity = latest_history.find_by(stock_id: history_params[:stock_id]).exist_quantity.to_i == 1 ? 0 : 1
+    save_quantity = history_params[:quantity].to_i == 1 ? 0 : 1
+    new_history = History.build(stock_id: params[:stock_id], exist_quantity: save_quantity)
 
-    new_history = @stock.histories.build(exist_quantity: save_quantity)
     if new_history.save
-      latest_history.reload
-      set_stock_reflected_latest_history(latest_history) # 更新後のlatest_historyを基に再セット
-
+      set_stock_reflected_latest(History.latest)
       broadcast.replace_stock(@stock)
       flash.now[:success] = t("defaults.flash_message.updated", item: t("defaults.models.history"))
       render turbo_stream: turbo_stream.update("flash", partial: "shared/flash_message")
@@ -27,10 +22,10 @@ class HistoriesController < ApplicationController
   private
 
   def history_params
-    params.permit(:stock_id)
+    params.permit(:stock_id, :quantity)
   end
 
-  def set_stock_reflected_latest_history(latest_history)
+  def set_stock_reflected_latest(latest_history)
     @stock = Stock.joins_latest_history(latest_history).find(history_params[:stock_id])
   end
 end
